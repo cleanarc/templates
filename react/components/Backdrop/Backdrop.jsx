5b9f43ebe8b65c29c80f9e9d9d68e320d1309c66
Deon Jackson Jr <deonj197@gmail.com>
2022-06-23 17:59:31 -0400

    remove react-gen templates

--- react/components/Backdrop/Backdrop.jsx
+++
@@ -1,7 +0,0 @@
-import styles from './<% .ComponentName %>.module.css';
-
-const <% .ComponentName %> = ({ onClose }) => (
-  <div className={styles.backdrop} onClick={onClose} />
-);
-
-export default <% .ComponentName %>;
